7213b948cb8653031936e662070b89a1204238b1
jenniferemanuele <jenniferemanuele950@gmail.com>
2023-11-25 22:05:45 -0300

    Conexoes e associacoes

--- src/main/java/demo/DAO/AlunoDAO.java
+++ src/main/java/demo/DAO/AlunoDAO.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-
 import demo.ententidades.Aluno;
 
 public class AlunoDAO {
@@ -45,7 +44,7 @@ public class AlunoDAO {
         connect();
         try {
 
-            String query = "INSERT INTO nome_aluno (nome, email, curso) VALUES (?, ?, ?)";
+            String query = "INSERT INTO aluno (nome_aluno, email_aluno, cursos_matriculados) VALUES (?, ?, ?)";
             try (PreparedStatement preparedStatement = jdbcConnection.prepareStatement(query)) {
                 preparedStatement.setString(1, aluno.getNome());
                 preparedStatement.setString(2, aluno.getEmail());
@@ -62,7 +61,7 @@ public class AlunoDAO {
     public void atualizarAluno(Aluno aluno) throws SQLException {
         connect();
         try {
-            String query = "UPDATE nome_aluno SET nome=?, email=?, curso=?";
+            String query = "UPDATE nome_aluno SET nome_aluno=?, email_aluno=?, cursos_matriculados=?";
             try (PreparedStatement preparedStatement = jdbcConnection.prepareStatement(query)) {
                 preparedStatement.setString(1, aluno.getNome());
                 preparedStatement.setString(2, aluno.getEmail());
@@ -76,7 +75,7 @@ public class AlunoDAO {
         }
     }
 
-    public List<Aluno> listarAlunos() throws SQLException {
+    public List<Aluno> listarAluno() throws SQLException {
         connect();
         List<Aluno> alunosList = new ArrayList<>();
 
@@ -86,9 +85,9 @@ public class AlunoDAO {
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     while (resultSet.next()) {
                         Aluno aluno = new Aluno(
-                                resultSet.getString("nome"),
-                                resultSet.getString("Email"),
-                                resultSet.getString("cursosMatriculados"));
+                                resultSet.getString("nome_aluno"),
+                                resultSet.getString("email_aluno"),
+                                resultSet.getString("cursos_matriculados"));
 
                         alunosList.add(aluno);
                     }
@@ -102,9 +101,11 @@ public class AlunoDAO {
     }
 
     public void matricularAlunoEmCurso(Aluno aluno, String curso) throws SQLException {
+      
         connect();
+
         try {
-            String query = "INSERT INTO matriculas (aluno_Id, nome_curso) VALUES (?, ?)";
+            String query = "INSERT INTO cursos (id_curso, nome_curso) VALUES (?, ?)";
             try (PreparedStatement preparedStatement = jdbcConnection.prepareStatement(query)) {
                 preparedStatement.setString(1, aluno.getNome());
                 preparedStatement.setString(2, curso);
@@ -120,7 +121,7 @@ public class AlunoDAO {
     public void cancelarMatriculaAluno(Aluno aluno, String curso) throws SQLException {
         connect();
         try {
-            String query = "DELETE FROM matriculas WHERE aluno_Id = ? AND nome_curso = ?";
+            String query = "DELETE FROM cursos WHERE id_curso = ? AND nome_curso = ?";
             try (PreparedStatement preparedStatement = jdbcConnection.prepareStatement(query)) {
                 preparedStatement.setString(1, aluno.getNome());
                 preparedStatement.setString(2, curso);
@@ -132,22 +133,21 @@ public class AlunoDAO {
             disconnect();
         }
     }
-
-    public void gerarRelatorioDesempenho() throws SQLException {
+ public static void gerarRelatorioDesempenho(Aluno aluno) throws SQLException {
         connect();
         try {
-            String query = "SELECT nome_aluno, matricula.curso_nome, notas.notas" +
-                    "FROM aluno" +
-                    "JOIN matricula ON nome_aluno = matricula.aluno_nome" +
-                    "LEFT JOIN notas ON matriculas.curso_nome " +
-                    "AND aluno.nome + notas.nome_aluno";
+            String query = "SELECT nome_aluno, matricula.id_curso, notas.nota " +
+                    "FROM aluno " +
+                    "JOIN matricula ON aluno.nome_aluno = matricula.id_aluno " +
+                    "LEFT JOIN notas ON matricula.id_curso = notas.id_curso " +
+                    "AND aluno.nome_aluno = notas.id_aluno";
 
             try (PreparedStatement preparedStatement = jdbcConnection.prepareStatement(query)) {
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     while (resultSet.next()) {
-                        String nomeAluno = resultSet.getString("nome");
-                        String nomeCurso = resultSet.getString("curso_nome");
-                        int nota = resultSet.getInt("notas");
+                        String nomeAluno = resultSet.getString("nome_aluno");
+                        String nomeCurso = resultSet.getString("id_curso");
+                        int nota = resultSet.getInt("nota");
 
                         System.out.print("Aluno: " + nomeAluno + " | Curso: " + nomeCurso + " | Nota: " + nota);
                     }
@@ -159,5 +159,4 @@ public class AlunoDAO {
             disconnect();
         }
     }
-
 }
--- src/main/java/demo/ententidades/App.java
+++ src/main/java/demo/ententidades/App.java
@@ -6,21 +6,27 @@ import demo.DAO.AlunoDAO;
 import demo.DAO.CursosDAO;
 import demo.DAO.ProfessorDAO;
 
-public class App{
-    public static void main(String[] args){
-        try{
+public class App {
+    public static void main(String[] args) {
+        try {
+            // Instanciar os DAOs
             AlunoDAO alunoDao = new AlunoDAO("jdbc:postgresql://localhost:3333/northwind", "postgres", "ihatethis19");
             ProfessorDAO professorDAO = new ProfessorDAO("jdbc:postgresql://localhost:3333/northwind", "postgres", "ihatethis19");
             CursosDAO cursosDAO = new CursosDAO("jdbc:postgresql://localhost:3333/northwind", "postgres", "ihatethis19");
 
-
+            // Instanciar um aluno
             Aluno aluno = new Aluno("sales", "sales@gmail.com", "ADS");
-            AlunoDAO.cadastrarAluno(aluno);
+
+            // Cadastrar o aluno usando o objeto alunoDao
+            alunoDao.cadastrarAluno(aluno);
+
+            // Gerar relatório de desempenho usando o objeto alunoDao
+            alunoDao.gerarRelatorioDesempenho(aluno);
 
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            // Se houver algum código que você precisa executar sempre, coloque aqui
         }
-        finally{}
-      
     }
 }
